e7d87da1f8b3e8aa249cfbdc5aac89f24adf5c01
rajverma328 <jprajverma328@gmail.com>
2024-08-22 05:04:42 +0530

    changing orientation timetable

--- FRONT_END/scripts.js
+++ FRONT_END/scripts.js
@@ -1,2 +1,26 @@
 // This can be used to add interactivity later
 console.log("Timetable loaded successfully!");
+
+const checkbox = document.getElementById("rc_switch");
+const element_orient = document.getElementById("orienter");
+function toggleClass(){
+    if (checkbox.checked) {
+        element_orient.classList.add("toggle0");
+        element_orient.classList.remove("toggle1");
+    } else {
+        element_orient.classList.add("toggle1");
+        element_orient.classList.remove("toggle0");
+    }
+}
+
+function checkDocumentWidth() {
+    if (window.innerWidth < 1300) {
+        checkbox.checked = false;
+        element_orient.classList.add("toggle1");
+        element_orient.classList.remove("toggle0");
+    }
+}
+
+// Run the function on window resize and when the page loads
+window.addEventListener('resize', checkDocumentWidth);
+window.addEventListener('load', checkDocumentWidth);
